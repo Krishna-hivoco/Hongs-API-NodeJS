56d3a28bdc053c2f178c7209f1b169d3ac1ba686
Krishna-hivoco <krishna@hivoco.com>
2024-10-16 23:24:41 +0530

    removing something poll

--- src/config/db.js
+++ src/config/db.js
@@ -3,15 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_DATABASE,
-  port: process.env.DATABASE_PORT || 3306,
-});
-
 export const getConnection = async () => {
-  const connection = await pool.getConnection();
+  const connection = await mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
+    port: process.env.DATABASE_PORT || 3306,
+  });
   return connection;
 };
