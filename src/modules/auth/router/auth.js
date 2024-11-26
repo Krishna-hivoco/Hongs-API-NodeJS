1151547a63fb06a539e4d6671e82800fcedf5673
Krishna-hivoco <krishna@hivoco.com>
2024-11-26 17:05:46 +0530

    remove auth

--- src/modules/auth/router/auth.js
+++ src/modules/auth/router/auth.js
@@ -4,14 +4,14 @@ import authService from "../services/auth.js";
 
 const router = Router();
 
-router.post(
-  "/register",
-  httpHandler(async (req, res) => {
-    const data = req.body;
-    const result = await authService.SignUp(data);
-    res.send(result);
-  })
-);
+// router.post(
+//   "/register",
+//   httpHandler(async (req, res) => {
+//     const data = req.body;
+//     const result = await authService.SignUp(data);
+//     res.send(result);
+//   })
+// );
 router.post(
   "/login",
   httpHandler(async (req, res) => {
